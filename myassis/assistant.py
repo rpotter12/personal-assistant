c02d1a718ab87d614dff1af5903f88638af27838
rpotter12 <rohitpotter12@gmail.com>
2019-07-30 22:13:44 +0530

    added browser search result

--- myassis/assistant.py
+++ myassis/assistant.py
@@ -4,6 +4,12 @@ import pyttsx3
 import os
 import spotdl
 
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
+
 recognizer = sr.Recognizer()
 microphone = sr.Microphone()
 
@@ -28,7 +34,7 @@ while True:
 
     # to tell what command is given
     if command != 'speak again':
-		engine.say(command)
+        engine.say(command)
 
     # to show google search result
     if "show result for " in command:
@@ -44,22 +50,36 @@ while True:
     	continue
 
     # to play songs
-    if "play " in command:
-    	command = command + ".mp3"
-    	engine.say(command)
-    	os.system(command)
-    	continue
+    if "play" in command:
+        command = command + ".mp3"
+        engine.say(command)
+        os.system(command)
+        continue
 
     # to download songs
     if "download song" in command:
-    	engine.say("which song you want to download")
-    	song = command_recog()
-    	songdownloadcommand = 'spotdl --song "'+song+'"'
-    	os.system(songdownloadcommand)
-    	continue
+        engine.say("which song you want to download")
+        song = command_recog()
+        songdownloadcommand = 'spotdl --song "'+song+'"'
+        os.system(songdownloadcommand)
+        continue
+
+    # to show search result in web browser
+    if "search" in command:
+        openwebbrowser(command)
 
 # method for google search result
 def googlesearchresult(wanttosearch):
 	query = wanttosearch
 	for j in search(query, tld="co.in", num=10, stop=1, pause=2): 
 		print(j) 
+
+def openwebbrowser(search):
+    driver = webdriver.Chrome(ChromeDriverManager().install())
+    driver.get("https://www.google.co.in/")
+    wait = WebDriverWait(driver, 600)
+
+    xpath = '//*[@id="tsf"]/div[2]/div/div[1]/div/div[1]/input'
+    search_area = wait.until(EC.presence_of_element_located((By.XPATH, xpath)))
+
+    search_area.send_keys(search + Keys.ENTER)
--- requirements.sh
+++ requirements.sh
@@ -4,12 +4,14 @@ sudo apt-get install sox libsox-fmt-all
 
 # speechrecognition library
 pip3 install SpeechRecognition
+pip3 install pyaudio
 
 pip3 install beautifulsoup4
 pip3 install google
 
 # for text to speech
 pip3 install pyttsx3
+pip3 install gtts
 
 # for downloading songs
 pip3 install spotdl
