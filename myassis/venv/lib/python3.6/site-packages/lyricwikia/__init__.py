a7ec46ba135f4e693c3021687717db6de59a9cd7
rpotter12 <rohitpotter12@gmail.com>
2019-10-20 05:34:45 +0530

    deleted environment

--- myassis/venv/lib/python3.6/site-packages/lyricwikia/__init__.py
+++
@@ -1,6 +0,0 @@
-from .lyricwikia import get_lyrics, get_all_lyrics
-from .lyricwikia import Song, Artist, LyricsNotFound
-from .version import __version__
-
-__all__ = ['get_lyrics', 'get_all_lyrics',
-           'Song', 'Artist', 'LyricsNotFound', '__version__']
